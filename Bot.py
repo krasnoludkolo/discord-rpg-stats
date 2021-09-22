9f6e8d2cab27ec011b401f7c3b167bc013f207c5
Danek <janek@projmen.pl>
2021-09-22 16:17:51 +0200

    Init

---
+++ Bot.py
@@ -0,0 +1,29 @@
+import asyncio
+import os
+
+from discord.ext import commands
+
+from cogs.RabbinCog import RabbinCog
+
+
+async def run():
+    bot = Bot()
+    await bot.start(os.environ.get('DISCORD_TOKEN'))
+
+
+class Bot(commands.Bot):
+    def __init__(self):
+        super().__init__(
+            command_prefix='!'
+        )
+        self.add_cog(RabbinCog(self))
+
+    async def on_ready(self):
+        print('Logged in as')
+        print(self.user.name)
+        print(self.user.id)
+        print('------')
+
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(run())
